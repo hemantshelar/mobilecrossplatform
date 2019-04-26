6723651582bdba9ac53ed22c44eea6d1d01ac057
Hemant Shelar <hemant.shelar@gmail.com>
2019-04-26 18:40:28 +1000

    basic working.

--- xPlatAuction/xPlatAuction/MainPage.xaml.cs
+++ xPlatAuction/xPlatAuction/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using xPlatAuction.Models;
 
 namespace xPlatAuction
 {
@@ -12,6 +14,24 @@ namespace xPlatAuction
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			this.message.Text = "test!!";
+			try
+			{
+				MobileServiceClient mobileServiceClient = new MobileServiceClient("https://xplatauctionhemant.azurewebsites.net");
+				var items = await mobileServiceClient.GetTable<TodoItem>().ReadAsync(); ;
+				var item = items.First();
+				this.message.Text = item.Text;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			
+			//this.message.Text = 
 		}
 	}
 }
---
+++ xPlatAuction/xPlatAuction/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace xPlatAuction.Models
+{
+	public class TodoItem 
+	{
+		public string Id { get; set; }
+
+		public string Text { get; set; }
+
+		public bool Complete { get; set; }
+	}
+}
